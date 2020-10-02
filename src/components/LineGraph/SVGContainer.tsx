a083a060b0b2f92f7811cf577dd0b12691a0bfbd
Joshua Gore <joshua_gore@eagle.co.nz>
2020-10-02 16:19:24 +1300

    more work on d3

--- src/components/LineGraph/SVGContainer.tsx
+++ src/components/LineGraph/SVGContainer.tsx
@@ -7,7 +7,26 @@ type Props = {
 
 const SVGContainer: React.FC<Props> = ({ children }: Props) => {
     const containerRef = React.useRef<HTMLDivElement>(null);
-    return <div ref={containerRef} style={{position: 'relative', width: '100%', height: '100%'}} ></div>;
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    const init = () => {
+        if (!containerRef.current) {
+            return;
+        }
+        const svgWidth = containerRef.current.offsetWidth;
+        const svgHeight = containerRef.current.offsetHeight;
+        const margin = { top: 10, right: 30, bottom: 30, left: 60 };
+        const graphWidth = svgWidth - margin.right - margin.left;
+        const graphHeight = svgHeight - margin.top - margin.bottom;
+        d3.select(containerRef.current).append('svg');
+    };
+    return (
+        <div
+            ref={containerRef}
+            style={{ position: 'relative', width: '100%', height: '100%' }}
+        ></div>
+    );
 };
 
 export default SVGContainer;
